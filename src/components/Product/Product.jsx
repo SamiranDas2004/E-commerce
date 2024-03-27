b8622c0d5b22215b7d86bdc03c285e11273b7439
Samiran_Das <samiran4209@gmail.com>
2024-03-27 16:25:45 +0530

    insurted the radio button

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,6 +1,11 @@
 import { Fragment, useState } from "react";
 import { Dialog, Disclosure, Menu, Transition } from "@headlessui/react";
 import { XMarkIcon } from "@heroicons/react/24/outline";
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
 import {
   ChevronDownIcon,
   FunnelIcon,
@@ -342,12 +347,14 @@ export default function Product() {
                     className="border-b border-gray-200 py-6"
                   >
                     {({ open }) => (
-                      <>
+                      <FormControl>
                         <h3 className="-my-3 flow-root">
                           <Disclosure.Button className="flex w-full items-center justify-between bg-white py-3 text-sm text-gray-400 hover:text-gray-500">
-                            <span className="font-medium text-gray-900">
+                            {/* <span className="font-medium text-gray-900">
                               {section.name}
-                            </span>
+                            </span> */}
+                            <FormLabel id="demo-radio-buttons-group-label">{section.name}</FormLabel>
+
                             <span className="ml-6 flex items-center">
                               {open ? (
                                 <MinusIcon
@@ -365,30 +372,23 @@ export default function Product() {
                         </h3>
                         <Disclosure.Panel className="pt-6">
                           <div className="space-y-4">
+                          <RadioGroup
+    aria-labelledby="demo-radio-buttons-group-label"
+    defaultValue="female"
+    name="radio-buttons-group"
+  >
                             {section.options.map((option, optionIdx) => (
-                              <div
-                                key={option.value}
-                                className="flex items-center"
-                              >
-                                <input
-                                  id={`filter-${section.id}-${optionIdx}`}
-                                  name={`${section.id}[]`}
-                                  defaultValue={option.value}
-                                  type="checkbox"
-                                  defaultChecked={option.checked}
-                                  className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
-                                />
-                                <label
-                                  htmlFor={`filter-${section.id}-${optionIdx}`}
-                                  className="ml-3 text-sm text-gray-600"
-                                >
-                                  {option.label}
-                                </label>
-                              </div>
+    <>
+     <FormControlLabel value={option.label} control={<Radio />} label={option.value} />
+
+    </>
+    
+
                             ))}
+                            </RadioGroup>
                           </div>
                         </Disclosure.Panel>
-                      </>
+                      </FormControl>
                     )}
                   </Disclosure>
                 ))}
