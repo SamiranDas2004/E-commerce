2cfdbc21656e66c7c9ae90eadea57b72006998cc
Samiran_Das <samiran4209@gmail.com>
2024-03-29 23:16:40 +0530

    ""

--- src/components/ProductDetails/ProductDetails.jsx
+++ src/components/ProductDetails/ProductDetails.jsx
@@ -22,6 +22,8 @@
 import { useState } from 'react'
 import { StarIcon } from '@heroicons/react/20/solid'
 import { RadioGroup } from '@headlessui/react'
+import {Button, Grid, Rating} from '@mui/material'
+import ProductReviewCart from './ProductReviewCart'
 
 const product = {
   name: 'Basic Tee 6-Pack',
@@ -55,14 +57,12 @@ const product = {
     { name: 'Black', class: 'bg-gray-900', selectedClass: 'ring-gray-900' },
   ],
   sizes: [
-    { name: 'XXS', inStock: false },
-    { name: 'XS', inStock: true },
+    
+    
     { name: 'S', inStock: true },
     { name: 'M', inStock: true },
     { name: 'L', inStock: true },
-    { name: 'XL', inStock: true },
-    { name: '2XL', inStock: true },
-    { name: '3XL', inStock: true },
+  
   ],
   description:
     'The Basic Tee 6-Pack allows you to fully express your vibrant personality with three grayscale options. Feeling adventurous? Put on a heather gray tee. Want to be a trendsetter? Try our exclusive colorway: "Black". Need to add an extra pop of color to your outfit? Our white tee has you covered.',
@@ -128,61 +128,43 @@ export default function ProductDetails() {
             />
           </div>
           <div className=" flex flex-wrap space-x-5 justify-center">
-            <div className="aspect-h-2 aspect-w-3 overflow-hidden rounded-lg">
+           {product.images.map((item)=><div className="aspect-h-2 aspect-w-3 overflow-hidden rounded-lg max-w-[5rem] max-h-[5rem] mt-4">
               <img
-                src={product.images[1].src}
-                alt={product.images[1].alt}
+                src={item.src}
+                alt={item.alt}
                 className="h-full w-full object-cover object-center"
               />
-            </div>
-            <div className="aspect-h-2 aspect-w-3 overflow-hidden rounded-lg">
-              <img
-                src={product.images[2].src}
-                alt={product.images[2].alt}
-                className="h-full w-full object-cover object-center"
-              />
-            </div>
-          </div>
-          <div className="aspect-h-5 aspect-w-4 lg:aspect-h-4 lg:aspect-w-3 sm:overflow-hidden sm:rounded-lg">
-            <img
-              src={product.images[3].src}
-              alt={product.images[3].alt}
-              className="h-full w-full object-cover object-center"
-            />
-          </div>
+            </div>) 
+           }
         </div>
 
+</div>
         {/* Product info */}
-        <div className="mx-auto max-w-2xl px-4 pb-16 pt-10 sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:grid-rows-[auto,auto,1fr] lg:gap-x-8 lg:px-8 lg:pb-24 lg:pt-16">
-          <div className="lg:col-span-2 lg:border-r lg:border-gray-200 lg:pr-8">
+        <div className="lg:col-span-1 mx-auto max-w-2xl px-4 pb-16 sm:px-6 lg:max-w-7xl lg:px-8 lg:pb-24">
+          <div className="lg:col-span-2 ">
             <h1 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">{product.name}</h1>
           </div>
 
           {/* Options */}
           <div className="mt-4 lg:row-span-3 lg:mt-0">
             <h2 className="sr-only">Product information</h2>
-            <p className="text-3xl tracking-tight text-gray-900">{product.price}</p>
+            {/* <p className="text-3xl tracking-tight text-gray-900">{product.price}</p> */}
 
+            <div className='flex space-x-5 items-center text-lg lg:text-xl text-gray-900 mt-6'>
+              <p className=' font-semibold'>199</p>
+              <p className="opacity-50 line-through ">266</p>
+              <p className=' text-green-600 font-semibold'>
+              5% Off
+              </p>
+            </div>
             {/* Reviews */}
             <div className="mt-6">
-              <h3 className="sr-only">Reviews</h3>
-              <div className="flex items-center">
-                <div className="flex items-center">
-                  {[0, 1, 2, 3, 4].map((rating) => (
-                    <StarIcon
-                      key={rating}
-                      className={classNames(
-                        reviews.average > rating ? 'text-gray-900' : 'text-gray-200',
-                        'h-5 w-5 flex-shrink-0'
-                      )}
-                      aria-hidden="true"
-                    />
-                  ))}
-                </div>
-                <p className="sr-only">{reviews.average} out of 5 stars</p>
-                <a href={reviews.href} className="ml-3 text-sm font-medium text-indigo-600 hover:text-indigo-500">
-                  {reviews.totalCount} reviews
-                </a>
+              <div className='flex items-center'>
+                <Rating name=" read-only" value={3.5} readOnly/>
+                <p className=' opacity-50 text-sm'>5660</p>
+                <p className='ml-3 text-sm font-medium text-indigo-600 hover:text-indigo-500'> 
+                3770 Reviews
+                </p>
               </div>
             </div>
 
@@ -285,12 +267,12 @@ export default function ProductDetails() {
                 </RadioGroup>
               </div>
 
-              <button
-                type="submit"
-                className="mt-10 flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-              >
-                Add to bag
-              </button>
+            <Button
+            variant='contained' 
+            sx={{px:"2rem",py:"1rem", bgcolor:"#9155fd"}}
+            >  Add to bag</Button>
+               
+              
             </form>
           </div>
 
@@ -327,6 +309,27 @@ export default function ProductDetails() {
             </div>
           </div>
         </div>
+        </section>
+
+
+
+{/* 
+    reviews and ratings*/}
+
+
+        <section>
+          <h1 className='font-semibold text-lg pb-4'>
+            Review's And Ratings
+          </h1>
+          <div className='border p-5 '>
+          <Grid container spacing={7}>
+            <Grid item xs={7}>
+              <div className='space-y-5'>
+              {[1,1,1].map((item)=><ProductReviewCart/>)}
+              </div>
+            </Grid>
+          </Grid>
+          </div>
         </section>
       </div>
     </div>
