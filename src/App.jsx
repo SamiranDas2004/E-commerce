01511732019842684e36ad216f0f8c6d2c8ea545
Samiran_Das <samiran4209@gmail.com>
2024-04-06 00:57:19 +0530

    ""

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Navbar from './components/Navbar/Navbar'
-import Homepage from './pages/Homepage/Homepage'
-import Footer from './components/Footer/Footer'
-import Product from './components/Product/Product'
-import ProductDetails from './components/ProductDetails/ProductDetails'
-import { Cable } from '@mui/icons-material'
-import Cart from './components/cart/Cart'
-
-
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+import Homepage from "./pages/Homepage/Homepage";
+import Footer from "./components/Footer/Footer";
+import Product from "./components/Product/Product";
+import ProductDetails from "./components/ProductDetails/ProductDetails";
+import { Cable } from "@mui/icons-material";
+import Cart from "./components/cart/Cart";
+import Checkout from "./components/Checkout/Checkout";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-    <div><Navbar/></div>
-{/* <div><Product/></div> */}
+      <div>
+        <Navbar />
+      </div>
+      {/* <div><Product/></div> */}
 
-{/* <ProductDetails/> */}
-<Cart/>
-    <div><Footer/></div>
+      {/* <ProductDetails/> */}
+      {/* <Cart/> */}
+      <Checkout/>
+      <div>
+        <Footer />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
