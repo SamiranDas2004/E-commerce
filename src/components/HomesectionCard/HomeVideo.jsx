2378154d220b31b5377b882ea487d81ca9eb784e
Yousuf <yousufali06296@gmail.com>
2024-05-22 00:26:35 +0530

    fixed ronaldo photo

--- src/components/HomesectionCard/HomeVideo.jsx
+++ src/components/HomesectionCard/HomeVideo.jsx
@@ -1,15 +1,21 @@
-import React from 'react';
+import React from "react";
 
 function HomeVideo() {
   return (
     <div className="grid grid-cols-2 gap-4 p-4">
       <div className="video-wrapper">
-        <img   
-      
-        src="https://pbs.twimg.com/media/ERZaFQ9W4AAyzEo.jpg" alt="Video 1" className="w-full h-custom object-cover"/>
+        <img
+          src="https://pbs.twimg.com/media/ERZaFQ9W4AAyzEo.jpg"
+          alt="Video 1"
+          className="w-full h-custom object-cover"
+        />
       </div>
       <div className="video-wrapper">
-        <img src="https://pbs.twimg.com/media/ERZaFQ9W4AAyzEo.jpg" alt="Video 2" className="w-full h-custom object-cover"/>
+        <img
+          src="https://img.olympics.com/images/image/private/t_s_16_9_g_auto/t_s_w1460/f_auto/primary/xritbykuhiha28mexocr"
+          alt="Video 2"
+          className="w-full h-full object-cover"
+        />
       </div>
     </div>
   );
