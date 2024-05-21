e37231500a8c3920eca12eedfd01372a4367121d
Yousuf <yousufali06296@gmail.com>
2024-05-21 23:55:45 +0530

    fixed payment gateway

--- src/components/ProductDetails/ProductDetails.jsx
+++ src/components/ProductDetails/ProductDetails.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { MensData } from "../../Data/mens";
-import { useState } from "react";
+
 import toast, { Toaster } from "react-hot-toast";
 
 function ProductDetails() {
@@ -11,71 +11,11 @@ function ProductDetails() {
   // setamount(selectedItem.price);
 
   // comment
-  const amount = selectedItem.price;
 
   const navigate = useNavigate();
 
   const handlePayment = async () => {
-    console.log(amount);
-    try {
-      const res = await fetch(`http://localhost:3000/api/payment/order`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          amount,
-        }),
-      });
-
-      const data = await res.json();
-
-      console.log(data);
-      handlePaymentVerify(data.data);
-      navigate(`/checkout/${id}`);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const handlePaymentVerify = async (data) => {
-    const options = {
-      key: "rzp_test_PH4sMZMaizjRIb",
-      amount: data.amount,
-      currency: data.currency,
-      name: "ActiveLife Store",
-      description: "Test Mode",
-      order_id: data.id,
-      handler: async (response) => {
-        console.log("response", response);
-        try {
-          const res = await fetch(`http://localhost:3000/api/payment/verify`, {
-            method: "POST",
-            headers: {
-              "content-type": "application/json",
-            },
-            body: JSON.stringify({
-              razorpay_order_id: response.razorpay_order_id,
-              razorpay_payment_id: response.razorpay_payment_id,
-              razorpay_signature: response.razorpay_signature,
-            }),
-          });
-
-          const verifyData = await res.json();
-
-          if (verifyData.message) {
-            toast.success(verifyData.message);
-          }
-        } catch (error) {
-          console.log(error);
-        }
-      },
-      theme: {
-        color: "#5f63b8",
-      },
-    };
-    const rzp1 = new window.Razorpay(options);
-    rzp1.open();
+    navigate(`/checkout/${id}`);
   };
 
   return (
